8e8a2e1cd7587b1fd4ee3cff30441b8db4b5c2e4
Niels Albarran <niels.albarran@hotmail.com>
2021-03-30 00:02:33 +0200

    constants

--- triple-x.cpp
+++ triple-x.cpp
@@ -5,5 +5,17 @@ int main()
     std::cout << "You are a secret agent breaking into a secure sever room...";
     std::cout << std::endl;
     std::cout << "Enter the correct code to continue...";
+
+    const int a = 4;
+    const int b = 3;
+    const int c = 2;
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
     return 0;
 }
